80b3fa44e273f930dc1407f63f2e1ad36927fe2b
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-03-28 08:58:46 -0600

    made my stubs and initialized my data members

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,5 +1,84 @@
 package art.view;
 
-public class ArtPanel {
+import java.awt.*;
+import java.util.Hashtable;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import art.controller.ArtController;
 
+public class ArtPanel extends JPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private ArtController app;
+	
+	private SpringLayout appLayout;
+	
+	private ShapeCanvas canvas;
+	
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton clearButton;
+	private JButton saveButton;
+	private JButton colorButton;
+	
+	private int currentScale;
+	private int currentEdgeCount;
+	
+	public ArtPanel(ArtController app)
+	{
+		super();
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MINIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new ShapeCanvas();
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0, 1));
+		
+		triangleButton = new JButton("Add Triangle");
+		rectangleButton = new JButton("Add Rectangle");
+		ellipseButton = new JButton("Add Ellipse");
+		polygonButton = new JButton("Add Polygon");
+		clearButton = new JButton("Clear Image");
+		saveButton = new JButton("Save Image");
+		colorButton = new JButton("Change Color");
+		
+		setupSlider();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void setupSliders()
+	{
+		
+	}
+	private void setupPanel()
+	{
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
 }
