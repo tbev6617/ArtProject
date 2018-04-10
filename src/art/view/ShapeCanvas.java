a04054d1c23a7563c936a8877c97dddd9ff6f3c3
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-04-10 09:56:08 -0600

    Worked on shapeCanvas

--- src/art/view/ShapeCanvas.java
+++ src/art/view/ShapeCanvas.java
@@ -17,20 +17,49 @@ public class ShapeCanvas extends JPanel
 {
 	private ArrayList<Polygon> triangleList;
 	private ArrayList<Polygon> polygonList;
-	private ArrayList<Polygon> ellipseList;
-	private ArrayList<Polygon> rectangleList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
 	private ArtController app;
 	
 	private BufferedImage canvasImage;
 	
 	public ShapeCanvas(ArtController app)
 	{
+		super();
+		this.app = app;
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		rectangleList = new ArrayList<Rectangle>();
 		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(600, 600));
+		this.setMaximumSize(getPreferredSize());
 	}
 	
 	public void addShape(Shape current)
 	{
-		
+		if (current instanceof Polygon)
+		{
+			if (((Polygon) current).xpoints.length == 3)
+			{
+				triangleList.add((Polygon) current);
+			}
+			else
+			{
+				polygonList.add((Polygon) current);
+			}
+		}
+		else if (current instanceof Ellipse2D)
+		{
+			ellipseList.add((Ellipse2D) current);
+		}
+		else
+		{
+			rectangleList.add((Rectangle) current);
+		}
+		updateImage();
 	}
 	
 	public void clear()
@@ -55,13 +84,42 @@ public class ShapeCanvas extends JPanel
 	
 	private void updateImage()
 	{
+		Graphics2D currentGraphics = (Graphics2D) canvasImage.getGraphics();
 		
+		for (Ellipse2D current : ellipseList)
+		{
+			currentGraphics.setColor(randomColor());
+			currentGraphics.setStroke(new BasicStroke(2));
+			currentGraphics.fill(current);
+			currentGraphics.setColor(randomColor());
+			currentGraphics.draw(current);
+		}
+		for (Polygon currentTriangle : triangleList)
+		{
+			currentGraphics.setColor(randomColor());
+			currentGraphics.fill(currentTriangle);
+		}
+		
+		for (Polygon currentPolygon : polygonList)
+		{
+			currentGraphics.setColor(randomColor());
+			currentGraphics.setStroke(new BasicStroke(4));
+			currentGraphics.draw(currentPolygon);
+		}
+		
+		for (Rectangle currentRectangle : rectangleList)
+		{
+			currentGraphics.setColor(randomColor());
+			currentGraphics.fill(currentRectangle);
+		}
+		currentGraphics.dispose();
+		repaint();
 	}
 	
+	@Override
 	protected void paintComponent(Graphics graphics)
 	{
-		
+		super.paintComponent(graphics);
+		graphics.drawImage(canvasImage, 0, 0, null);
 	}
-	
-	
 }
